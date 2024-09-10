abd3e7143fb4f3dce9d791cc5d818d0fa8070f2e
Sergey Tushinsky <67974097+Tushinsky@users.noreply.github.com>
2024-09-10 17:16:09 +0300

    Add files via upload

---
+++ src/main/java/com/mycompany/bookwebapp/RegisterServlet.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bookwebapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Sergii.Tushinskyi
+ */
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+
+    private static final String query = 
+            "INSERT INTO book_data(book_title, book_edition, price) VALUES(?,?,?)";
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet RegisterServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet RegisterServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            try {
+                /* TODO output your page here. You may use following sample code. */
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(RegisterServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            String bookTitle = request.getParameter("bookName");
+            String bookEdition = request.getParameter("bookEdition");
+            float bookPrice = Float.parseFloat(request.getParameter("bookPrice"));
+            try(Connection connection = DriverManager.getConnection("jdbc:mysql:///book_app", "root", "masterkey");
+                    PreparedStatement ps = connection.prepareStatement(query)) {
+                ps.setString(1, bookTitle);
+                ps.setString(2, bookEdition);
+                ps.setFloat(3, bookPrice);
+                int row = ps.executeUpdate();
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<title>Servlet RegisterServlet</title>");            
+                out.println("<link rel='stylesheet' href='../java/styles/style.css'");
+                out.println("</head>");
+                out.println("<body>");
+                out.println("<div class='list'>");
+                if(row > 0) {
+                    out.println("<h2>Книга зарегистрирована</h2>");
+                } else {
+                    out.println("<h2>Регистрация книги не удалась</h2>");
+                }
+                out.println("<br><a href='home.html'>Главная</a>");
+                out.println("<br><a href='booklist'>Список книг</a>");
+                out.println("</div>");
+                out.println("</body>");
+                out.println("</html>");
+            } catch(SQLException ex) {
+                Logger.getLogger(RegisterServlet.class.getName()).log(Level.SEVERE, null, ex);
+                out.println("<h2>" + ex.getMessage() + "</h2>");
+            }
+            
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
